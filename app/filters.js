bb06f53189a1fd0c76b1a39714d6c991b528be9b
Ismail Ismail <ismail.ismail9@nhs.net>
2024-04-18 20:42:11 +0100

    nofilters fix

--- app/filters.js
+++ app/filters.js
@@ -7,27 +7,5 @@ module.exports = function (env) { /* eslint-disable-line no-unused-vars */
    */
   const filters = {};
 
-  // import moment from 'moment';
-
-  // filters.momento = string => {
-  //   return moment(string, 'DD/MM/YYYY', true).format())
-  // }
-
-  var dateFilter = require('nunjucks-date-filter');
-  filters.dateFull = string => {
-    return dateFilter(string, 'dddd, Do MMMM YYYY')
-  }
-
-  filters.shortDate = string => {
-    return dateFilter(string, 'Do MMMM')
-  }
-
-  filters.when = string => {
-    return dateFilter(string, 'ddd,Do MMMM hh:mm')
-  }
-
-
-
-
   return filters;
 };
