6e2b99c9a60f391d081af884ff91e738b3ff7c23
Ismail Ismail <ismail.ismail9@nhs.net>
2024-04-19 08:07:51 +0100

    Commit before first share

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'Vaccine Select and Invite Manager',
+  serviceName: 'Manage a vaccination campaign',
 
   // Port to run nodemon on locally
   port: 2000,
